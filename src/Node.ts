d936053786796e60db4ffef7c28527238c72a429
diljitsgit <diljits1776@gmail.com>
2025-02-14 17:02:05 +0530

    Adding some features to the node object

--- src/Node.ts
+++ src/Node.ts
@@ -3,12 +3,17 @@ import { Canvas, Group, Rect, Shadow, Textbox } from "fabric";
 export default function addNode(canvas: Canvas | null) {
     const nodeWidth = 180;
     const nodeHeight = 50;
+    let realWidth = 180;
+    let realHeight = 180;
+    let textScaleX = 1;
+    let textScaleY = 1;
     const rounded = 5;
     const defaultShadowOffset = 5;
     const defaultAccentWidth = 15;
     const defaultTextSize = 16;
     const padding = 5;
     const defaultStrokeWidth = 1;
+    let textEditing = false;
 
     const brutalShadow = new Shadow({
         color: "#000",
@@ -35,7 +40,7 @@ export default function addNode(canvas: Canvas | null) {
         ry: rounded,
         left: 0,
         top: 0,
-        fill: "red",
+        fill: "#FF0000",
     });
 
     const clipRect = new Rect({
@@ -49,7 +54,8 @@ export default function addNode(canvas: Canvas | null) {
 
     const text = new Textbox("Node1", {
         fontSize: defaultTextSize,
-        fill: "black",
+        fill: "#000",
+        backgroundColor: "red",
         top: nodeHeight / 2,
         left: defaultAccentWidth + padding,
         width: nodeWidth - defaultAccentWidth - 2 * padding,
@@ -57,44 +63,125 @@ export default function addNode(canvas: Canvas | null) {
         originY: "center",
         textAlign: "center",
         strokeUniform: true,
+        scaleX: 1,
     });
 
     const nodeGroup = new Group([mainRect, highlightRect, text], {
         left: 425,
         top: 300,
+        lockScalingFlip: true,
+    });
+
+    text.on("changed", () => {
+        console.log(text.height);
+        console.log(mainRect.height);
+        mainRect.set({
+            width:
+                (text.width + (defaultAccentWidth + 2 * padding)) / textScaleX,
+        });
+        if (text.height > mainRect.height) {
+            const changeHeight =
+                (text.height + 2 * padding + 2 * defaultStrokeWidth) /
+                textScaleY;
+            mainRect.set({
+                height: changeHeight,
+            });
+            highlightRect.set({
+                height: changeHeight,
+            });
+            clipRect.set({
+                top: -changeHeight / 2,
+                height: changeHeight,
+            });
+            text.set({
+                top: (defaultTextSize + padding + defaultStrokeWidth) / 2,
+            });
+        }
+        nodeGroup.setCoords();
+        canvas?.requestRenderAll();
     });
 
     nodeGroup.on("mousedblclick", () => {
-        text.enterEditing();
-        console.log(nodeGroup.aCoords);
+        if (textEditing) {
+            text.exitEditing();
+            textEditing = !textEditing;
+        } else {
+            text.enterEditing();
+            textEditing = !textEditing;
+        }
+    });
+
+    nodeGroup.on("deselected", () => {
+        if (textEditing) {
+            text.exitEditing();
+            textEditing = !textEditing;
+        }
     });
 
     nodeGroup.on("scaling", () => {
-        nodeGroup._objects[0].set({
-            shadow: {
-                offsetX: defaultShadowOffset / nodeGroup.scaleX,
-                offsetY: defaultShadowOffset / nodeGroup.scaleY,
-            },
-            rx: rounded / nodeGroup.scaleX,
-            ry: rounded / nodeGroup.scaleY,
-        });
-        nodeGroup._objects[1].set({
-            rx: rounded / nodeGroup.scaleX,
-            ry: rounded / nodeGroup.scaleY,
-        });
-        nodeGroup._objects[2].set({
-            fontSize: defaultTextSize / nodeGroup.scaleX,
-            top: nodeHeight / 2 / nodeGroup.scaleY / defaultTextSize,
-        });
-        clipRect.set({
-            width: defaultAccentWidth / nodeGroup.scaleX,
-            height: nodeGroup.height,
-            left: -nodeWidth / 2 - defaultStrokeWidth,
-            top: -nodeHeight / 2 - defaultStrokeWidth,
-        });
-        clipRect.setCoords();
-        highlightRect.setCoords();
+        realWidth = nodeWidth * nodeGroup.scaleX;
+        realHeight = nodeHeight * nodeGroup.scaleY;
+        textScaleX = nodeGroup.scaleX;
+        textScaleY = nodeGroup.scaleY;
+        if (textEditing) {
+            text.exitEditing();
+            textEditing = !textEditing;
+        }
+        if (realHeight >= 28 && realWidth >= 100) {
+            mainRect.set({
+                shadow: {
+                    offsetX: defaultShadowOffset / nodeGroup.scaleX,
+                    offsetY: defaultShadowOffset / nodeGroup.scaleY,
+                },
+                rx: rounded / nodeGroup.scaleX,
+                ry: rounded / nodeGroup.scaleY,
+            });
+            highlightRect.set({
+                rx: rounded / nodeGroup.scaleX,
+                ry: rounded / nodeGroup.scaleY,
+            });
+            text.set({
+                scaleX: 1 / nodeGroup.scaleX,
+                scaleY: 1 / nodeGroup.scaleY,
+                width:
+                    nodeGroup.width * textScaleX -
+                    (defaultAccentWidth + 2 * padding + 2 * defaultStrokeWidth),
+                top: nodeHeight / 2 / defaultTextSize,
+                left:
+                    -(nodeGroup.width / 2) +
+                    (defaultAccentWidth + padding) / nodeGroup.scaleX,
+            });
+            clipRect.set({
+                width: defaultAccentWidth / nodeGroup.scaleX,
+                height: mainRect.height + defaultStrokeWidth,
+                left: -nodeWidth / 2 - defaultStrokeWidth,
+                top: -mainRect.height / 2 - defaultStrokeWidth,
+            });
+        } else {
+            if (realHeight <= 28) {
+                nodeGroup.set({
+                    scaleY: 28 / nodeGroup.height,
+                });
+            }
+            if (realWidth <= text.width) {
+                nodeGroup.set({
+                    scaleX: text.width / nodeGroup.width,
+                });
+            }
+            if (realHeight <= text.height) {
+                nodeGroup.set({
+                    scaleY: text.height / nodeGroup.height,
+                });
+            }
+            if (realWidth <= 100) {
+                nodeGroup.set({
+                    scaleX: 100 / nodeGroup.width,
+                });
+            }
+        }
     });
 
-    canvas?.add(nodeGroup);
+    if (canvas) {
+        canvas.add(nodeGroup);
+    }
 }
